c828142d978edf9f1301832160aeab32e8cbc449
Nguyen Tran Huy Viet <huyyvietnguyentran@gmail.com>
2025-03-08 15:52:36 +0700

    Done compare with scikit learn

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -6,21 +6,24 @@ class LinearRegression:
         self.weights = None # a vector where number of features taken = number of element example: [w1, w2, w3] for y = w1a + w2b+ w3c + bias
         self.bias = None # a scalar
     
-    def gradient_descent(self, X, y):
+    def gradient_descent_fit(self, X, y):
         n_samples, n_features = X.shape
         self.weights = np.zeros(n_features)
         self.bias = 0
 
         for _ in range(self.iterations):
-            y_predicted = np.dot(X, self.weights) + self.bias # an array/vector of y predicted for each samples
-            dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y)) * 2 # an array/vector of derivative for each features
-            db = (1 / n_samples) * np.sum(y_predicted - y) * 2 # a scalar of bias
+            y_predicted = np.dot(X, self.weights) + self.bias # an array/vector of y predicted for each samples. Dimension: (n_samples, 1)
+            # when making calculations with scalar and vector, boardcasting happens
+            # .dot when facing 2d array is vector multiplication
+            dw = (2 / n_samples) * np.dot(X.T, (y_predicted - y)) # an array/vector of derivative for each features. Dimension: (n_features, 1)
+            db = (2 / n_samples) * np.sum(y_predicted - y) # a scalar of bias
 
-            self.weights -= dw * self.learning_rate
-            self.bias -= db * self.learning_rate
+            self.weights -= (dw * self.learning_rate)
+            self.bias -= (db * self.learning_rate)
 
     def predict(self, X):
-        return np.dot(X, self.weights) + self.bias # an array/vector of y predicted for each samples
+        y_approximated = np.dot(X, self.weights) + self.bias # an array/vector of y predicted for each samples
+        return y_approximated, self.weights, self.bias
     
-    def mean_squared_error(y_true, y_pred):
-        return np.mean((y_true - y_pred) ** 2)
+
+    
--- linear_regression/linear_regression_test.py
+++ linear_regression/linear_regression_test.py
@@ -1,11 +1,11 @@
 import numpy as np
-from sklearn import datasets
+from sklearn import datasets, linear_model
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
+# cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 
 # iris = pd.read_csv('./Iris.csv')
 
@@ -19,24 +19,50 @@ cmap = ListedColormap(['#FF0000', '#00FF00', '#0000FF'])
 # plt.scatter(X[:, 1], X[:, 2], c = y_encoded, cmap = cmap, edgecolors = 'k', s = 20)
 # plt.show()
 
-penguins = pd.read_csv('./penguins.csv')
-penguins.dropna()
-X = penguins[['flipper_length_mm']].values
-y = penguins['body_mass_g'].values
+# iris = pd.read_csv('./iris.csv')
+# iris = iris.dropna()
+# X = iris[['SepalWidthCm']].values
+# y = iris['SepalLengthCm'].values
+
+car = pd.read_csv('./car_price_dataset.csv')
+car = car.dropna()
+min_index = 0
+max_index = 100
+X = car.loc[:100, ['Mileage']].values
+y = car.loc[:100, 'Price'].values
+
+X_all = car.loc[max_index:, ['Mileage']].values
+y_all = car.loc[max_index:, 'Price'].values
+
+X, y = datasets.make_regression(
+    n_samples=200, n_features=1, noise=40, random_state=100
+)
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=20)
 
+def mean_squared_error(y_true, y_pred):
+    return np.mean((y_true - y_pred) ** 2)
 
 from linear_regression import LinearRegression
 
 regressor = LinearRegression()
-regressor.gradient_descent(X_train, y_train)
-y_predictions = regressor.predict(X)
+regressor.gradient_descent_fit(X_train, y_train)
+y_predictions, scratch_weights, scratch_bias = regressor.predict(X_test)
 
-# plt.scatter(X[:, 0], y, color='b', marker='o' , s = 30)
-cmap = plt.get_cmap("viridis")
-fig = plt.figure(figsize=(8, 6))
+regressor_scikit_learn = linear_model.LinearRegression()
+regressor_scikit_learn.fit(X_train, y_train)
+print("scikit-learn’s solution : weight = ", scratch_weights, "bias = ", scratch_bias)
+print("our solution : weight = ", regressor_scikit_learn.coef_[0], "bias = ", regressor_scikit_learn.intercept_)
+
+mse_value = mean_squared_error(y_test, y_predictions)
+print(mse_value)
+
+
+y_pred_line, predicted_weights, predicted_bias = regressor.predict(X)
+cmap = plt.get_cmap('viridis')
+fig = plt.figure(figsize=(10,6))
 m1 = plt.scatter(X_train, y_train, color=cmap(0.9), s=10)
 m2 = plt.scatter(X_test, y_test, color=cmap(0.5), s=10)
-m3 = plt.scatter(X[:, 0], y, color='b', marker='o' , s = 30)
-plt.plot(X, y_predictions, color="black", linewidth=2, label="Prediction")
+m3 = plt.scatter(X_all, y_all, color=cmap(0.5), s=10)
+plt.plot(X, y_pred_line, color="black", linewidth=2, label="Prediction")
 plt.show()
